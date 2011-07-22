9e8ad2659c5fde664ca436974228cec8821e100c
David Middleton <david.middleton@gmail.com>
2011-07-22 17:41:05 +1000

    WIP

--- lib/devise-pki/schema.rb
+++ lib/devise-pki/schema.rb
@@ -1,8 +1,8 @@
 module DevisePKI
   module Schema
     def pkikey
-      apply_devise_schema :pki_pub_key,Text
-      apply_devise_schema :pki_priv_key_enc,Text
+      apply_devise_schema :pki_pub_key,String
+      apply_devise_schema :pki_priv_key_enc,String
     end
   end
 end
